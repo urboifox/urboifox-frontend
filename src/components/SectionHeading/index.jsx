d6823f2bc6b95c3c164620685ee830a7301d9612
Fox <mohamed.dev.egy@gmail.com>
2023-08-19 06:05:18 +0200

    fixed heading text color not working

--- src/components/SectionHeading/index.jsx
+++ src/components/SectionHeading/index.jsx
@@ -7,7 +7,7 @@ const SectionHeading = ({ text }) => {
     <div
       className={`${
         darkTheme ? "text-light" : "text-dark"
-      }block md:self-start max-w-[80rem] tracking-wide w-full mx-auto mb-32 font-main font-thin link px-5 text-2xl md:text-5xl lg:text-5xl capitalize`}
+      } block md:self-start max-w-[80rem] tracking-wide w-full mx-auto mb-32 font-main font-thin link px-5 text-2xl md:text-5xl lg:text-5xl capitalize`}
     >
       <h2>{text}</h2>
     </div>
