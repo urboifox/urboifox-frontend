bc4788febdbedb7cb1cc452a16ab534786b4a0fb
Fox <mohamed.dev.egy@gmail.com>
2023-09-12 06:22:56 +0200

    added particles js

---
+++ src/components/Particlesjs/index.jsx
@@ -0,0 +1,80 @@
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+export default function Particlesjs() {
+  const options = {
+    background: {
+      color: {
+        value: "none",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 150,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#555",
+      },
+      links: {
+        color: "#555",
+        distance: 150,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 1,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 30,
+      },
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 2 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return <Particles id="tsparticles" init={particlesInit} options={options} />;
+}
